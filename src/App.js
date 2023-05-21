9b4152ce3fed6c89f55f2b1e01830c404342dd5c
vistrate <ioan.vlad.istrate02@gmail.com>
2023-05-21 19:12:39 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect,useState } from 'react';
+import Login from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon
+}
+from 'mdb-react-ui-kit';
+import "./styles.css"
+
+function Login() {
+  return (
+    <MDBContainer fluid>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
+
+          <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <MDBBtn outline className='mx-2 px-5' color='white' size='lg' onClick={() => console.log("sugpl")}>
+                Login
+              </MDBBtn>
+
+              
+
+              
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default Login;
